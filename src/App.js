b20748f665566ba2c7c76b6f4eed35ae39d9db1f
perrushow <47873707+perrushow@users.noreply.github.com>
2021-06-30 21:37:24 -0300

    Arquivos src

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Timeline from './components/Timeline';
+import './App.css';
+
+const App = () => (
+  <>
+   <h1> Experiências relevantes G|P </h1>
+   <Timeline />
+   </>
+);
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,42 @@
+const data = [
+        {
+            text: 'Empresa de T.I que desenvolve e vende produtos de software proprietários para Social Listenign e Market Research',
+            date: '2016',
+            
+            category: {
+                tag: 'eCGlobal Solutions',
+                color: '#66EA70'
+            },
+            link: {
+                url: 'http://127.0.0.1:5500/Portfolio/TimeLineGP/index.html',
+                text: 'Mais sobre a experiência'
+            }
+        },
+        {
+            text: 'Empresa de T.I que desenvolve e vende produtos de software proprietários para Social Listening e Market Research',
+            date: '2017',
+            category: {
+                tag: 'eCGlobal Solutions',
+                color: '#e17b77'
+            },
+            link: {
+                url: 'http://127.0.0.1:5500/Portfolio/TimeLineGP/index.html/',
+                text: 'Mais sobre a experiência'
+            }
+        },
+        {
+            text: 'Empresa responsável por gerenciar projetos multidiciplinares no universo industrial',
+            date: '2019-2020',
+            category: {
+                tag: 'MAPROTEC Engenharia',
+                color: '#1DA1F2'
+            },
+            link: {
+                url: 'http://127.0.0.1:5500/Portfolio/TimeLineGP/index.html',
+                text: 'Mais sobre a experiência'
+            }
+        },
+        
+    ]
+
+export default data;
